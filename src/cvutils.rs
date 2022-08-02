183b1c6d16a7d30f187298b3e4aa1119bf99ac70
jer <alphastrata@gmail.com>
2022-08-02 15:55:06 +0800

    working

--- src/cvutils.rs
+++ src/cvutils.rs
@@ -5,16 +5,12 @@ use opencv::{core, imgcodecs, prelude::*};
 use std::collections::HashMap;
 use std::path::Path;
 
-use crate::user_config;
+use crate::user_config::Config;
 use crate::{fileutils::cleanup_tmp, himawaridt::HimawariDatetime, tiles::LocalTile};
 
 const ROWMAX: u32 = 20;
 const COLMAX: u32 = 20;
 
-pub(crate) enum Axis {
-    X,
-    Y,
-}
 /// load image with opencv
 /// #Arguments:
 /// * `&str` path to load image from
@@ -33,7 +29,7 @@ pub(crate) async fn cv_load_image(p: &str) -> Result<core::Mat> {
 pub(crate) async fn assemble_full_disc(
     m: HashMap<(u32, u32), LocalTile>,
     hwdt: HimawariDatetime,
-    uc: &user_config::Config,
+    uc: &Config,
 ) -> Result<crate::wallpaperutils::FullDisc> {
     let mut working_vec = vec![];
     let mut range: Vector<Mat> = Vector::new();
@@ -51,10 +47,10 @@ pub(crate) async fn assemble_full_disc(
                     .clone(),
             );
         }
-        range.push(cv_concat_array(&working_vec, Axis::X, Some(true)).await?); //NOTE: is this Some(bool) sloppy?
+        range.push(cv_concat_array(&working_vec, uc, Some(true)).await?); //NOTE: is this Some(bool) sloppy?
         working_vec.clear();
     }
-    concat_off_axis(range, Axis::Y, hwdt).await?;
+    concat_off_axis(range, uc, hwdt).await?;
 
     let p = std::path::Path::new(&uc.completed).join(hwdt.pretty_filename());
     assert!(cleanup_tmp(uc)?); //NOTE Cleanup the completed too?
@@ -62,28 +58,27 @@ pub(crate) async fn assemble_full_disc(
     crate::wallpaperutils::FullDisc::new(&p)
 }
 
-// TODO: Consider removing the Axis args, as you're not giving the user a choice really...
 /// Wrapping OpenCV's array/matrix based concats, to build disks from LocalTiles
 /// #Arguments:
 /// * `v` a Vector<LocalTile> which represent a tile on disk you're looking to concatenate.
 /// * `axis` the axis you want to concat on.
 /// * `keep_tmps` - a bool to determine if you keep intermediate files or not during the concatenation.
-async fn cv_concat_array(v: &Vec<LocalTile>, axis: Axis, keep_tmps: Option<bool>) -> Result<Mat> {
+async fn cv_concat_array(v: &Vec<LocalTile>, uc: &Config, keep_tmps: Option<bool>) -> Result<Mat> {
     let mut range: Vector<Mat> = Vector::new(); //opencv requires you use their vector type, which is NOT a rust Vec.
     let mut mat = core::Mat::default();
 
     for lt in v.iter() {
-        //TODO this could be a oneliner no? .iter().map(|lt| lt.....
+        // TODO: oneliner
         range.push(cv_load_image(lt.path_as_str()).await?);
     }
-    match axis {
-        Axis::X => core::vconcat(&range, &mut mat).unwrap(),
-        Axis::Y => core::hconcat(&range, &mut mat).unwrap(),
-    }
+
+    core::vconcat(&range, &mut mat).unwrap();
+
     // keep the intermediate representations on disk...
     if keep_tmps.unwrap_or(false) {
-        let filename = format!("completed/{}complete.png", v[0].get_xy().await.0);
-        imgcodecs::imwrite(&filename, &mat, &types::VectorOfi32::new())?;
+        let filename = format!("{}/{}complete.png", uc.completed, v[0].get_xy().await.0);
+        let _ = imgcodecs::imwrite(&filename, &mat, &types::VectorOfi32::new()).unwrap();
+        dbg!(&filename);
     }
     Ok(mat)
 }
@@ -91,16 +86,14 @@ async fn cv_concat_array(v: &Vec<LocalTile>, axis: Axis, keep_tmps: Option<bool>
 /// Wrapping OpenCV's array/matrix based concat but taking a native cpp vector of Mat's, as opporesed to a rust Vec. (see: cv_concat_array)
 /// #Arguments:
 /// * `range` a Vector<Mat> of the images you want to concat, NOTE: this is the cpp wrapped Vector, not the rust one
-/// * `axis` the axis you want to concat on
 /// * `hwdt` the HimawariDatetime used to create all the tiles this img will be made up of
-async fn concat_off_axis(range: Vector<Mat>, axis: Axis, hwdt: HimawariDatetime) -> Result<()> {
+async fn concat_off_axis(range: Vector<Mat>, uc: &Config, hwdt: HimawariDatetime) -> Result<()> {
     let mut mat = core::Mat::default();
-    match axis {
-        Axis::X => core::vconcat(&range, &mut mat).unwrap(),
-        Axis::Y => core::hconcat(&range, &mut mat).unwrap(),
-    }
-    let filename = format!("completed/{}", hwdt.pretty_filename());
-    let _ = imgcodecs::imwrite(&filename, &mat, &types::VectorOfi32::new()).unwrap();
+    core::hconcat(&range, &mut mat).unwrap();
+    dbg!(hwdt.pretty_filename());
+    let filename = format!("{}/{}", uc.completed, hwdt.pretty_filename());
+    dbg!(&filename);
+    let _ = imgcodecs::imwrite(&filename, &mut mat, &types::VectorOfi32::new()).unwrap();
     Ok(())
 }
 
